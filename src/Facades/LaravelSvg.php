9cc5ba370e22e01bd6f9a1a01fe8a2d64eb5344a
Abdullah Karam <abdallakaramdev@gmail.com>
2023-07-14 15:36:22 +0300

    add documention @method svgFor() to Facade

--- src/Facades/LaravelSvg.php
+++ src/Facades/LaravelSvg.php
@@ -4,6 +4,9 @@ namespace Abdullah\LaravelSvg\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Abdullah\LaravelSvg\Services\LaravelSvg svgFor(string $userFullName)
+ */
 class LaravelSvg extends Facade
 {
     protected static function getFacadeAccessor(): string
